c1d657a8c3e5b6f7089ad7934c1d39d6605660df
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2022-05-30 19:15:26 -0500

    Bug de table

--- src/components/People/Person/index.js
+++ src/components/People/Person/index.js
@@ -9,39 +9,41 @@ export const Person = ({
   return (
     <div className="seccion__table ">
       <h1>People</h1>
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Height</th>
-            <th>Mass</th>
-            <th>Hair color</th>
-            <th>Birth year</th>
-            <th>Plus info</th>
-          </tr>
-        </thead>
-        <tbody>
-          {fetchData &&
-            fetchData.map((person) => {
-              return (
-                <tr className="contenido__tabla" key={person.name}>
-                  <td>{person.name}</td>
-                  <td>{person.height}</td>
-                  <td>{person.mass}</td>
-                  <td>{person.hair_color}</td>
-                  <td>{person.birth_year}</td>
-                  <td>
-                    <i
-                      id={person.name}
-                      onClick={getValueModal}
-                      className="fa-solid fa-plus"
-                    ></i>
-                  </td>
-                </tr>
-              )
-            })}
-        </tbody>
-      </table>
+      <div className="container__tabla">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Height</th>
+              <th>Mass</th>
+              <th>Hair color</th>
+              <th>Birth year</th>
+              <th>Plus info</th>
+            </tr>
+          </thead>
+          <tbody>
+            {fetchData &&
+              fetchData.map((person) => {
+                return (
+                  <tr className="contenido__tabla" key={person.name}>
+                    <td>{person.name}</td>
+                    <td>{person.height}</td>
+                    <td>{person.mass}</td>
+                    <td>{person.hair_color}</td>
+                    <td>{person.birth_year}</td>
+                    <td>
+                      <i
+                        id={person.name}
+                        onClick={getValueModal}
+                        className="fa-solid fa-plus"
+                      ></i>
+                    </td>
+                  </tr>
+                )
+              })}
+          </tbody>
+        </table>
+      </div>
       <div className="group__btns">
         <button className="btn" onClick={previPagination}>
           Previous
